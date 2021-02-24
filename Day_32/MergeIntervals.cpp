5390590a286582cdade6f37cb045155853590a30
DEEPAK KUMAR <62848415+dpkk1@users.noreply.github.com>
2021-02-24 21:07:22 +0530

    Create MergeIntervals.cpp
    question Link: https://leetcode.com/problems/merge-intervals/

---
+++ Day_32/MergeIntervals.cpp
@@ -0,0 +1,26 @@
+
+
+//Code: 
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0){
+            return intervals;
+        }
+        sort(intervals.begin(), intervals.end());  //O(n*log(n)) time complexity
+        
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+      
+      for(int i=1;i<intervals.size();i++){
+        if(ans.back()[1] >= intervals[i][0]){
+          ans.back()[1] = (ans.back()[1]>intervals[i][1] ? ans.back()[1] : intervals[i][1]);
+        }
+        else if(ans.back()[1] < intervals[i][0]){
+          ans.push_back(intervals[i]);
+        }
+      }
+      return ans;
+    }
+};
